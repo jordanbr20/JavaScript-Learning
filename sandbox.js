c71019fb5f2eaf7230d64fa9ac78efb7faddcf0b
Jordan Riley <jordanbr1528@gmail.com>
2022-11-14 20:06:34 -0500

    Added a reset button :)

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
 const number = document.querySelector('.number');
 const add = document.querySelector('.add');
 const sub = document.querySelector('.sub');
+const reset = document.querySelector('.reset');
 
 console.log(+number.innerHTML.toString() + 1);
 
@@ -14,4 +15,8 @@ sub.addEventListener('click', (e) =>{
     let newnumber = +number.innerHTML.toString() - 1;
     number.innerHTML = newnumber;
     //console.log(number.innerHTML.toString());
+})
+
+reset.addEventListener('click',(e) =>{
+    number.innerHTML = '0';
 })
